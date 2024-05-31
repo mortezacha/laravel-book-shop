6229c66154c3de22bcc65fed75c17297d4c6659e
mori_cha <mortezachabooshi@gmail.com>
2024-05-31 21:33:56 +0430

    add mybooks to navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('my_books.index')" :active="request()->routeIs('my_books.index')">
+                        {{ __('My Books') }}
+                    </x-nav-link>
                 </div>
             </div>
 
