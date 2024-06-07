d9608602cdd8e52b8a1f54b9a2dbef583e719f81
mori_cha <mortezachabooshi@gmail.com>
2024-06-07 21:36:26 +0430

    relation between Books and likes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
@@ -11,17 +13,20 @@ class Book extends Model
 
     protected $guarded = [];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-
-    protected function getFullImageUrlAttribute(){
-        if ($this->image_url){
+    public function likes():HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+    protected function getFullImageUrlAttribute()
+    {
+        if ($this->image_url) {
             return asset('storage/'.$this->image_url);
         }
+
         return null;
     }
-
-
 }
