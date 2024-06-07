250cf3436258851960681b3858f0a1367f590f41
mori_cha <mortezachabooshi@gmail.com>
2024-06-07 20:32:29 +0430

    single page to show book method

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 class TimelineController extends Controller
 {
     public function index(){
-        $books = Book::latest()->paginate(10);
+        $books = Book::latest()->paginate(20);
         return view('welcome',compact('books'));
+    }
+
+    public function show(Book $book)
+    {
+        return view('show',compact('book'));
     }
 }
